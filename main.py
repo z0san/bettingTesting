1feecd0e814250459394b1784d5aa32b9c15686f
Zohar Singer <injapanthree@gmail.com>
2020-01-13 10:21:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+
+from bs4 import BeautifulSoup
+import requests
+
+url = input("Please coppy and past the url of the data you would like to extract:  ")
+
+
+'''with open("test.html", encoding="utf8") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+'''
+
+page_response = requests.get(
+    url,
+    timeout=5,
+    headers={
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+    }
+)
+
+#print(page_response.content)
+
+
+soup = BeautifulSoup(page_response.content, "html.parser")
+
+#print(soup)
+
+data = []
+
+odds = soup.find("tbody", {"id": "t1"}).find_all('td')
+
+
+horses = []
+horseData = []
+
+
+i = 0
+while (i < len(odds)):
+    if (odds[i]["class"][0] == "sel"):
+        horses.append(odds[i])
+        odds.remove(odds[i])
+    elif ("data-bk" in odds[i].attrs):
+        currentHorseOdds = []
+        while (i < len(odds) and odds[i]["class"][0] != "sel"):
+
+            if ("data-bk" in odds[i].attrs):
+                currentHorseOdds.append({"data-bk": odds[i]["data-bk"], "odds": odds[i].p})
+
+            i+= 1
+
+        horseData.append(currentHorseOdds)
+    else:
+        i += 1
+
+
+
+
+'''for i in odds:
+    if ("data-bk" in i.attrs):
+        print(i)'''
+
+print()
+
+for i in range(len(horses)):
+    data.append({"name": horses[i].a["data-name"], "odds": horseData[i]})
+
+for i in data:
+    print(i)
