ace0fe5072f9886f55413b65ad0158da8b2a3f5e
Zohar <zinger@seas.upenn.edu>
2020-01-29 10:51:32 -0500

    made some progress parsing the json file

--- arbChecker.py
+++ arbChecker.py
@@ -1,7 +1,31 @@
-from testData import returnTestData
+import json
 
-data = returnTestData()
+with open('odds.json') as json_file:
+    data = json.load(json_file)
 
-for sport in data:
-    for i in sport:
-        print(i)
+
+
+def parseFrac(frac):
+    breaker = frac.find('/')
+    if(breaker != -1):
+        return int(frac[:breaker]) / int(frac[breaker + 1:]) + 1
+    return int(frac) + 1
+
+
+for sportNames in data:
+    print("checking", sportNames)
+    for sport in data[sportNames]:
+
+        events = list(sport.keys())
+        for eventName in events:
+            best = {}
+            if(type(sport[eventName]) is list):
+                for option in sport[eventName]:
+                    if(len(option["odds"]) > 0):
+                        max = 0
+                        for odds in option["odds"]:
+                            if( not odds["odds"] == None):
+                                decOdds = parseFrac(odds["odds"])
+                                if decOdds > max:
+                                    max = decOdds
+                        print("Max odds", max)
